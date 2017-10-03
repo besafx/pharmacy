b47cee37708d0baef45409f5ea3df1a574ce28f0
Development <islamhaker@gmail.com>
2017-10-03 22:07:10 +0300

    Deploy

--- src/main/java/com/besafx/app/schedule/ScheduleDailyOrders.java
+++ src/main/java/com/besafx/app/schedule/ScheduleDailyOrders.java
@@ -29,7 +29,7 @@ public class ScheduleDailyOrders {
     @Autowired
     private EmailSender emailSender;
 
-    @Scheduled(cron = "0 0 22 * * *")
+    @Scheduled(cron = "0 0/30 22 * * *")
     public void run() throws Exception {
         Future<byte[]> work = asyncScheduleDailyOrders.getFile();
         byte[] fileBytes = work.get();
