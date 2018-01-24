65fc4886d7e50f1ed7e74750ed100090551d069a
Development <islamhaker@gmail.com>
2018-01-24 11:01:45 +0300

    NONE

--- src/test/java/com/besafx/app/MainTests.java
+++ src/test/java/com/besafx/app/MainTests.java
@@ -1,13 +1,9 @@
 package com.besafx.app;
 
-import com.besafx.app.schedule.ScheduleWatchBillSells;
-import com.besafx.app.schedule.ScheduleWatchOrders;
-import com.besafx.app.service.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -17,33 +13,8 @@ public class MainTests {
 
     private final Logger log = LoggerFactory.getLogger(MainTests.class);
 
-    @Autowired
-    private ScheduleWatchOrders scheduleWatchOrders;
-
-    @Autowired
-    private ScheduleWatchBillSells scheduleWatchBillSells;
-
-    @Autowired
-    private OrderReceiptService orderReceiptService;
-
-    @Autowired
-    private BillSellReceiptService billSellReceiptService;
-
-    @Autowired
-    private ReceiptService receiptService;
-
-    @Autowired
-    private OrderService orderService;
-
-    @Autowired
-    private BillSellService billSellService;
-
-    @Autowired
-    private FundService fundService;
-
     @Test
     public void contextLoads() throws Exception {
-        scheduleWatchBillSells.runMonthly();
-        Thread.sleep(333333333);
+
     }
 }
